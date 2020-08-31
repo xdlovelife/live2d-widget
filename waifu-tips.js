9f58bea3c8ff978050db8dbd4a1737954f426468
xdlovelife <394794072@qq.com>
2020-08-31 13:10:04 +0800

    update

--- waifu-tips.js
+++ waifu-tips.js
@@ -19,7 +19,7 @@ function loadWidget(config) {
 	sessionStorage.removeItem("waifu-text");
 	document.body.insertAdjacentHTML("beforeend", `<div id="waifu">
 			<div id="waifu-tips"></div>
-			<canvas id="live2d" width="800" height="800"></canvas>
+			<canvas id="live2d" width="500" height="500"></canvas>
 			<div id="waifu-tool">
 				<span class="fa fa-lg fa-comment"></span>
 				<span class="fa fa-lg fa-paper-plane"></span>
