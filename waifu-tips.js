bf7a247f8154388e485338c7cd85a8e2b508c0f2
xdlovelife <394794072@qq.com>
2020-08-31 13:23:11 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -22,7 +22,6 @@ function loadWidget(config) {
 			<canvas id="live2d" width="80" height="80"></canvas>
 			<div id="waifu-tool">
 				<span class="fa fa-lg fa-comment"></span>
-				<span class="fa fa-lg fa-paper-plane"></span>
 				<span class="fa fa-lg fa-user-circle"></span>
 				<span class="fa fa-lg fa-street-view"></span>
 				<span class="fa fa-lg fa-camera-retro"></span>
